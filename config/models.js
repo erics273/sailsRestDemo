ed1e2fff844464cfdec9c7de60ceb697b6707394
Eric Schwartz <support@addteq.com>
2016-09-10 02:03:27 -0400

    altering drop behavior

--- config/models.js
+++ config/models.js
@@ -28,6 +28,6 @@ module.exports.models = {
   *                                                                          *
   ***************************************************************************/
 schema: true,
-migrate: 'drop'
+migrate: 'alter'
 
 };
